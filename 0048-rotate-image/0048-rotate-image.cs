0b60929157639554abf717340a28feeb52d4adcc
Muhammed saheen <99274222+muhammed-saheen@users.noreply.github.com>
2024-12-12 11:53:47 +0530

    Time: 0 ms (100%), Space: 46.1 MB (30.27%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public void Rotate(int[][] matrix) {
+        for(int i=0;i<matrix.Length;i++){
+            for(int j=i+1;j<matrix.Length;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.Length; i++) {
+            Array.Reverse(matrix[i]);
+        }  
+    }
+}
