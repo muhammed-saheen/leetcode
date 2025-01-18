57da86a35e515224bc47d0ad12de1f74715f614f
Muhammed saheen <99274222+muhammed-saheen@users.noreply.github.com>
2025-01-18 16:43:16 +0530

    Time: 36 ms (49.13%), Space: 69.1 MB (43.52%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+        HashSet<int> set = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++) {
+            if (set.Contains(nums[i])) {
+                return true;
+            }
+
+            set.Add(nums[i]);
+
+            if (set.Count > k) {
+                set.Remove(nums[i - k]);
+            }
+        }
+
+        return false;
+    }
+}
