506f6459435eef9cb13d9c8a36c862aa3e2c7f64
Muhammed saheen <99274222+muhammed-saheen@users.noreply.github.com>
2025-01-29 15:32:55 +0530

    Time: 0 ms (100%), Space: 42.3 MB (71.14%) - LeetHub

---
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int CountSeniors(string[] details) {
+     int count=0;
+     for(int i=0;i<details.Length;i++){
+        if(((details[i][11]-'0')*10+(details[i][12]-'0'))>60){
+            count++;
+        }
+     } 
+     return count ;  
+    }
+}
