a5ae2bdd37685e174ee76533fe6c3b6d126cab24
Muhammed saheen <99274222+muhammed-saheen@users.noreply.github.com>
2025-03-11 12:31:51 +0530

    Time: 26 ms (27.03%), Space: 43.1 MB (62.16%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cs
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cs
@@ -6,12 +6,11 @@ public class Solution {
         };
 
         for (int right = 0; right < s.Length; right++) {
-            dic[s[right]]++; // Expand window by adding s[right]
+            dic[s[right]]++;
 
-            // Shrink window from left when all characters are present
             while (dic['a'] > 0 && dic['b'] > 0 && dic['c'] > 0) {
-                count += s.Length - right; // Count all valid substrings
-                dic[s[left]]--; // Shrink window
+                count += s.Length - right; 
+                dic[s[left]]--;
                 left++; 
             }
         }
